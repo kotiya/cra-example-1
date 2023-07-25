bdf0d548e248c087d87ea55972316366e830c5af
Weiran Li <weiran@second.dev>
2023-07-25 11:56:50 -0700

    Second[Agent] cra to next.js

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import '../index.css'
+import '../App.css'
+import Head from 'next/head'
+
+export default function MyApp({ Component, pageProps}) {
+  return (
+    <>
+      <Head>
+        <title>React App</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      
+      <Component {...pageProps} />
+    </>
+  )
+}
